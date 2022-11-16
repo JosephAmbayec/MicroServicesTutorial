02f0b37db0602c15356667dbd9d0853b9a326a35
JosephAmbayec <joseph.ambayec20@alumni.loyola.ca>
2022-11-16 15:13:17 -0500

    trying chapter 3 video 1

---
+++ ECommerce.Api.Search/Services/SearchService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Api.Search.Interfaces;
+
+namespace ECommerce.Api.Search.Services
+{
+    public class SearchService : ISearchService
+    {
+        public async Task<(bool IsSuccess, dynamic SearchResults)> SearchAync(int customerId)
+        {
+            await Task.Delay(1);
+            return (true, new { Message = "Hello" });
+        }
+    }
+}
