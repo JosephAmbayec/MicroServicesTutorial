22c448c9d114b381ea7a13d8108645dceca44053
JosephAmbayec <44503159+JosephAmbayec@users.noreply.github.com>
2022-11-27 12:21:09 -0500

    completed challenge

--- ECommerce.Api.Search/Services/SearchService.cs
+++ ECommerce.Api.Search/Services/SearchService.cs
@@ -11,16 +11,19 @@ namespace ECommerce.Api.Search.Services
     {
         private readonly IOrdersService _orderService;
         private readonly IProductsService _productsService;
+        private readonly ICustomersService _customersService;
 
-        public SearchService(IOrdersService ordersService, IProductsService productsService)
+        public SearchService(IOrdersService ordersService, IProductsService productsService, ICustomersService customersService)
         {
             _orderService = ordersService;
             _productsService = productsService;
+            _customersService = customersService;
         }
         public async Task<(bool IsSuccess, dynamic SearchResults)> SearchAync(int customerId)
         {
             var ordersResult = await _orderService.GetOrdersAsync(customerId);
             var productsResult = await _productsService.GetProductsAsync();
+            var customersResult = await _customersService.GetCustomerAsync(customerId);
 
             if (ordersResult.isSuccess)
             {
@@ -28,11 +31,12 @@ namespace ECommerce.Api.Search.Services
                 {
                     foreach (var item in order.Items)
                     {
-                        item.ProductName = productsResult.Products.FirstOrDefault(p => p.Id == item.ProductId).Name;
+                        item.ProductName = productsResult.IsSuccess ? productsResult.Products.FirstOrDefault(p => p.Id == item.ProductId).Name : "Product info is not availible.";
                     }
                 }
                 var result = new
                 {
+                    Customer = customersResult.IsSuccess ? customersResult.Customer : new { Name = "Customer info is not availible." },
                     Orders = ordersResult.Orders
                 };
                 return (true, result);
--- ECommerce.Api.Search/Startup.cs
+++ ECommerce.Api.Search/Startup.cs
@@ -30,6 +30,8 @@ namespace ECommerce.Api.Search
             services.AddScoped<ISearchService, SearchService>();
             services.AddScoped<IOrdersService, OrdersService>();
             services.AddScoped<IProductsService, ProductsService>();
+            services.AddScoped<ICustomersService, CustomersService>();
+
             services.AddHttpClient("OrdersService", config =>
             {
                 config.BaseAddress = new Uri(Configuration["Services:Orders"]);
@@ -38,6 +40,10 @@ namespace ECommerce.Api.Search
             {
                 config.BaseAddress = new Uri(Configuration["Services:Products"]);
             }).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
+            services.AddHttpClient("CustomersService", config =>
+            {
+                config.BaseAddress = new Uri(Configuration["Services:Customers"]);
+            }).AddTransientHttpErrorPolicy(c => c.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
             services.AddControllers();
         }
 
